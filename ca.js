139f1ed0a7b6270949ad5f9470b693a2492ba303
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-08-21 00:05:02 +0300

    newfunctional

---
+++ ca.js
@@ -0,0 +1,17 @@
+// let a = +prompt('first', 4);
+// let b = +prompt('second', 2);
+// // if (a > b) {
+// //     console.log(a - b)
+// // } else {
+// //     throw (err)
+// // }
+// alert(a + b)
+
+const x = +prompt('chekushka', 4)
+const y = +prompt('polyshka', 2)
+
+ if (y > x) {
+     alert('ненаю')
+ } else {
+    alert(x - y)
+ } 
