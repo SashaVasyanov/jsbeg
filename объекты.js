2e1973bed8e76a5fb115445c2e259bf98acef721
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-08-27 18:45:57 +0300

    Objects+

--- объекты.js
+++ объекты.js
@@ -1,12 +1,15 @@
 const person = {
-    name: 'Dasha',
-    age: 16,
-    isModel: true,
-    langueages: ['ru', 'eng', 'de', 'fra'],
-//     greet() {
-// console.log('привет от меня')
-    }
-
+  name: "Dasha",
+  age: 16,
+  isModel: true,
+  langueages: ["ru", "eng", "de", "fra"],
+  info() {
+    console.info("Всё о покупателе:", this.name);
+    console.log("это", this);
+  },
+  //     greet() {
+  // console.log('привет от меня')
+};
 
 // console.log(person.name)
 // console.log(person.age)
@@ -26,17 +29,64 @@ const person = {
 //     console.log('ключ', keyyz)
 //     console.log('value', person[keyyz])
 // }
-//     }      
+//     }
 
 // const keys = Object.keys(person)
 // console.log(keys)
 
-Object.keys(person).forEach((key) => {
-    console.log('key',typeof key)
-    console.log('value', person[key])
-})
+// Object.keys(person).forEach((key) => {
+//     console.log('key',typeof key)
+//     console.log('value', person[key])
+// })
 
+// const key = 'name'
+// console.log(person[key])
 
-const key = 'name'
-console.log(person[key])
+// CONTEXT
 
+// person.info()
+
+// const logger = {
+//     keys(obj) {
+//         console.log('Object K ', Object.keys(obj))
+//     }
+// }
+// logger.keys(person)
+
+const logger = {
+  keys() {
+    console.log("Object K ", Object.keys(this));
+  },
+  KeysAndValue() {
+    Object.keys(this).forEach((key) => {
+      console.log(key, this[key]);
+    });
+  },
+  withParams(top = false, between = false, bottom = true) {
+    if (top) {
+      console.log("sssssssssssSssssssss");
+    }
+    Object.keys(this).forEach((key) => {
+      console.log(key, this[key]);
+      if (between) {
+        console.log("========================");
+      }
+      if (bottom) {
+        console.log("PPPPPPPPPPPPPP");
+      }
+    });
+  },
+};
+
+logger.withParams.call(person, true)
+// const bound = logger.keys.bind(person)
+// bound()
+
+// logger.keys.call(person)
+// logger.KeysAndValue.call(person)
+
+// function sayHello(firstName, lastName) {
+//   console.log(`Hello, {firstName} {lastName}`);
+// }
+
+// sayHello("Alex", "NAV"); // Hello, Alex NAV
